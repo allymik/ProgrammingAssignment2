5493ef3d7f08c15f394d1f0e2b22aa33e1739c81
allymik <mikhaylova.aleksandra@gmail.com>
2016-06-12 22:21:20 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,15 @@
-## This function sets the value of an inverse matrix to a different global environment
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() x
-  setinv <- function(solve) inv <<- inv
-  getinv <- function() inv
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
+
 }
 
-##set makes y into x, so set()
-##get just returns x
-##setinv invokes the function solve and the inverse it spits out is cached
 
+## Write a short comment describing this function
 
-##  This function checks if there is a value for an inverse matrix stored in cache and if there isn't, solves for an inverse matrix otherwise 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  inv <- x$getinv()
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data, ...)
-  x$setinv(inv)
-  inv
+        ## Return a matrix that is the inverse of 'x'
 }
